97f4110c066c117c0fab5a2a40b5396c5c36d2f6
yinqianzheng <yinqianzheng@gmail.com>
2020-11-01 16:31:37 -0800

    fix config key path for production

--- app.js
+++ app.js
@@ -12,7 +12,7 @@ const path = require("path");
 
 let db;
 if (process.env.NODE_ENV === "production") {
-  db = require("./config/keys_prod").mongoURI;
+  db = require("./config/prodKeys").mongoURI;
   app.use(express.static("client/build"));
   app.get("*", (req, res) => {
     res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
