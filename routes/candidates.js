b95610692a7b1a6f01e403d660a515a07d693e80
yinqianzheng <yinqianzheng@gmail.com>
2020-11-01 16:28:00 -0800

    remove rimraf

--- routes/candidates.js
+++ routes/candidates.js
@@ -1,9 +1,6 @@
 const router = require("express").Router();
 const Candidate = require("../models/Candidate");
 const fileupload = require("express-fileupload");
-const fs = require('fs');
-const path = require("path");
-const rimraf = require("rimraf");
 const uploadFile = require("../utils/uploadFile");
 const validateAddCandidateInput = require("../validation/addCandidate");
 
